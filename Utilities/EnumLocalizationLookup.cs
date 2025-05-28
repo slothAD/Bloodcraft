b64f0e5e9843fea37d83da2110d66aa21fc122f1
slothAD <sloth283116@gmail.com>
2025-05-29 04:16:37 +0800

    Update EnumLocalizationLookup.cs

--- Utilities/EnumLocalizationLookup.cs
+++ Utilities/EnumLocalizationLookup.cs
@@ -5,13 +5,14 @@ namespace Bloodcraft.Utilities.EnumLocalization;
 public static class EnumLocalizationLookup
 {
     public static string GetWeaponTypeZh(WeaponType type)
-        => EnumLocalization.WeaponTypeToZh.TryGetValue(type.ToString(), out var name) ? name : type.ToString();
+        => EnumLocalization.WeaponTypeToZh.TryGetValue(type, out var name) ? name : type.ToString();
 
     public static string GetBloodTypeZh(BloodType type)
-        => EnumLocalization.BloodTypeToZh.TryGetValue(type.ToString(), out var name) ? name : type.ToString();
+        => EnumLocalization.BloodTypeToZh.TryGetValue(type, out var name) ? name : type.ToString();
 
     public static string GetProfessionZh(ProfessionType type)
-        => EnumLocalization.ProfessionTypeToZh.TryGetValue(type.ToString(), out var name) ? name : type.ToString();
+        => EnumLocalization.ProfessionTypeToZh.TryGetValue(type, out var name) ? name : type.ToString();
+
 
     // 注意：這些是遊戲內建 enum，使用 .ToString() 作為查找
     public static string GetWeaponStatZh(Enum statType)
